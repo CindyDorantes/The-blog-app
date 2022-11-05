4457f99ca25303c6078322d5f90f7cee4a75169f
CindyDorantes <cindym.dorantes@gmail.com>
2022-11-04 20:59:24 -0600

    add users custom methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :posts
   has_many :likes
+
+  def last_three_post
+    Post.last(3)
+    # Post.order(created_at: :desc).limit(3)
+  end
 end
