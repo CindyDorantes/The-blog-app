2a4fd91dd2e4b6104267af39c0f95c986db898a0
CindyDorantes <cindym.dorantes@gmail.com>
2022-11-05 13:38:59 -0600

    add validations for the user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,16 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validation :title, presence: true, length: { maximum: 250 }
+  validation :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validation :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def last_five_comments
     comments.last(5)
   end
 end
+
+# Title must not be blank.
+# Title must not exceed 250 characters.
+# CommentsCounter must be an integer greater than or equal to zero.
+# LikesCounter must be an integer greater than or equal to zero.
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :posts
   has_many :likes
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_three_post
     Post.last(3)
