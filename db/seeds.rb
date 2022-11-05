e1237f608ea87bc0793dfc02f2710338479d53f2
CindyDorantes <cindym.dorantes@gmail.com>
2022-11-04 21:25:26 -0600

    add posts custom methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello again', text: 'This is my second post. Welcome')
+third_post = Post.create(author: first_user, title: 'Hi', text: 'Whats up folks?')
+fourth_post = Post.create(author: first_user, title: 'See you later', text: 'With this, I say goodbye')
+
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
+Comment.create(post: first_post, author: second_user, text: 'Interesting')
+Comment.create(post: first_post, author: second_user, text: 'I dont agree')
+Comment.create(post: first_post, author: second_user, text: 'I would like to know more')
+Comment.create(post: first_post, author: second_user, text: 'Thanks for sharing')
+Comment.create(post: first_post, author: second_user, text: 'Is that all?')
