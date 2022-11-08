644e0c97b0abbc57c10dcb0e8d1d6c07240e2fd2
CindyDorantes <cindym.dorantes@gmail.com>
2022-11-08 10:20:27 -0600

    create route, action and view for URL /posts and /posts/:id

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
+  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:user_id/posts/:id', to: 'posts#show'
 end
