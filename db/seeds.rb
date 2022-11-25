6a2911120cb274383c9fbc2696416f0985e084fd
CindyDorantes <cindym.dorantes@gmail.com>
2022-11-25 12:55:39 -0600

    fix information displayed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-first_user = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1665686310934-8fab52b3821b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d29tYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60', bio: 'Teacher from Poland.')
+
+first_user = User.create(email: 'tom@gmail.com', password: '123abc', name: 'Tom', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+                         bio: 'Teacher from Mexico.')
+second_user = User.create(email: 'lilly@gmail.com', password: '123abc', name: 'Lilly', photo: 'https://images.unsplash.com/photo-1665686310934-8fab52b3821b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d29tYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+                          bio: 'Teacher from Poland.')
+User.create(name: 'Cindy', email: 'cindy@gmail.com', password: '123abc', role: 'admin', bio: 'The blog Creator',
+            photo: 'https://picsum.photos/200')
 
 first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
 second_post = Post.create(author: first_user, title: 'Hello again', text: 'This is my second post. Welcome')
 third_post = Post.create(author: first_user, title: 'Hi', text: 'Whats up folks?')
-fourth_post = Post.create(author: first_user, title: 'See you later', text: 'With this, I say goodbye')
+fourth_post = Post.create(author: second_user, title: 'See you later', text: 'With this, I say goodbye')
 
 Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
 Comment.create(post: first_post, author: second_user, text: 'Interesting')
